437538ed3b5e21131844f7407ca32f76005f320e
SNatt <natsalnikova1976@gmail.com>
2022-12-20 20:45:33 +0300

    initial

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '5830146886:AAFSWbKyVuKcsF_94S6NnzH0BylMo1DIXBo'
+
+currencies = {
+              'Рубль'  : 'RUB',
+              'Евро'   : 'EUR',
+              'Доллар' : 'USD'
+             }
---
+++ extensions.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from config import currencies
+
+class APIException(Exception):
+    pass
+
+class CurrencyExchange:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+        try:
+            quote_ticker = currencies[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = currencies[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[currencies[base]]
+
+        return total_base
